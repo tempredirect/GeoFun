056a8a886ce5547ddad6b8b167c64a0425f867a6
Gareth Davis <gadavis@expedia.com>
2014-10-04 18:08:16 +0100

    get basedir for active property list from the launchconfig

--- src/main/java/com/ean/geofun/search/GeoSearchModule.java
+++ src/main/java/com/ean/geofun/search/GeoSearchModule.java
@@ -3,6 +3,7 @@ package com.ean.geofun.search;
 import com.ean.geofun.ActivePropertyList;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import ratpack.launch.LaunchConfig;
 
 import javax.inject.Singleton;
 import java.io.IOException;
@@ -14,13 +15,11 @@ import java.nio.file.Paths;
 public class GeoSearchModule extends AbstractModule {
 
   @Override
-  protected void configure() {
-
-  }
+  protected void configure() { }
 
   @Singleton
   @Provides
-  public Search search() throws IOException {
-    return new GeoHashSearch(new ActivePropertyList(Paths.get("ActivePropertyList.txt")));
+  public Search search(LaunchConfig launchConfig) throws IOException {
+    return new GeoHashSearch(new ActivePropertyList(launchConfig.getBaseDir().file("ActivePropertyList.txt")));
   }
 }
